b61fcb58325e1517da692384b789e76f4eb8b825
iain plimmer <iainplimmer@iains-iMac.local>
2016-12-11 11:39:50 +0000

    More promises added.

--- index.js
+++ index.js
@@ -5,54 +5,56 @@ var browserSync = require("browser-sync").create('gangle');
 
 'use strict'
 
+console.log('-------------------------------------');
+console.log('Gangle 0.0.1 Started.')
+console.log('-------------------------------------');
+
 var excludedDirectories = ['.//.git','.//node_modules'];  //  We need to know which directories to include
 
 var filelist = [];
-getDirectories('./', filelist, excludedDirectories)
+var promiseList = [];
+GetDirectories('./', filelist, excludedDirectories)
     .then(function (watchList) {
         watchList.forEach(function (dir) {
-            WatchFolder(dir);
+            WatchFolder(dir)
+            .then(function (e){
+                console.log(e)
+            })
         });
     })
 
 
-//console.log(watchList)
-//watchList.forEach(function (dir) {
-//    WatchFolder(dir);
+//  Initialise Browser sync now
+//browserSync.init({
+//    server: ".",
+//    port: 9001
 //});
 
-/*
-//  Initialise Browser sync now
-browserSync.init({
-    server: ".",
-    port: 9001
-});
-*/
+console.log('-------------------------------------');
+console.log('Gangle 0.0.1 Complete.')
+console.log('-------------------------------------');
 
-// function CompareNumbers (a, b) {
-//     return new Promise(function executePromise (resolve, reject) {  
-//         if (a && b && a === b) {                
-//             resolve('The values are the same!');
-//         } else {
-//             reject('Nope, the values are different');
-//         }
-//     });
-// };
-
-//  First thing we are going to do is setup a watcher on a particular folder, this will read the
-//  gangle.config.js configuration file 
+////////////////////////////////////////////////////////////////////////////////////////////////
+//  Setup a watcher on a particular folder, 
+//  TO DO : this will read the gangle.config.js configuration file for files to ignore 
+////////////////////////////////////////////////////////////////////////////////////////////////
 function WatchFolder (folderToWatch) {
-    console.log('Watching folder: ' + folderToWatch);
-    fs.watch(folderToWatch, {encoding: 'buffer'}, (eventType, filename) => {  
-        if (filename) {
-            console.log('filename \'' + filename + '\'has changed');
-            browserSync.reload();     
-        }
+    return new Promise(function executePromise (resolve, reject) {
+        fs.watch(folderToWatch, {encoding: 'buffer'}, (eventType, filename) => {  
+            if (filename) {
+                console.log('filename \'' + filename + '\'has changed');
+                browserSync.reload();     
+            }
+        });
+        resolve('Watching folder: ' + folderToWatch)
     });
 }
 
-//  We traverse the directory tree and add all directories to a list to watch
-function getDirectories (dir, filelist, exclude) {
+////////////////////////////////////////////////////////////////////////////////////////////////
+//  We traverse the directory tree and add all directories to a list to watch, returns a 
+//  promise so that we can perform this action async
+////////////////////////////////////////////////////////////////////////////////////////////////
+function GetDirectories (dir, filelist, exclude) {
     return new Promise(function executePromise (resolve, reject) { 
         var files = fs.readdirSync(dir);
         var i = exclude.indexOf(dir)
@@ -60,7 +62,7 @@ function getDirectories (dir, filelist, exclude) {
         if (i == -1) {
             files.forEach(function (file) {
             if (fs.statSync(dir + '/' + file).isDirectory()) {
-                getDirectories(dir + '/' + file, filelist, exclude)
+                GetDirectories(dir + '/' + file, filelist, exclude)
                     .then(function () {
                         resolve(filelist);
                     });
