7bd47024fc561275dc39070757119084d5155619
iain plimmer <iainplimmer@iains-iMac.local>
2016-12-11 11:30:32 +0000

    Promise implementation on the directory listing.

--- index.js
+++ index.js
@@ -8,10 +8,18 @@ var browserSync = require("browser-sync").create('gangle');
 var excludedDirectories = ['.//.git','.//node_modules'];  //  We need to know which directories to include
 
 var filelist = [];
-var watchList = getDirectories('./', filelist, excludedDirectories);
-watchList.forEach(function (dir) {
-    WatchFolder(dir);
-});
+getDirectories('./', filelist, excludedDirectories)
+    .then(function (watchList) {
+        watchList.forEach(function (dir) {
+            WatchFolder(dir);
+        });
+    })
+
+
+//console.log(watchList)
+//watchList.forEach(function (dir) {
+//    WatchFolder(dir);
+//});
 
 /*
 //  Initialise Browser sync now
@@ -20,6 +28,16 @@ browserSync.init({
     port: 9001
 });
 */
+
+// function CompareNumbers (a, b) {
+//     return new Promise(function executePromise (resolve, reject) {  
+//         if (a && b && a === b) {                
+//             resolve('The values are the same!');
+//         } else {
+//             reject('Nope, the values are different');
+//         }
+//     });
+// };
 
 //  First thing we are going to do is setup a watcher on a particular folder, this will read the
 //  gangle.config.js configuration file 
@@ -35,20 +53,25 @@ function WatchFolder (folderToWatch) {
 
 //  We traverse the directory tree and add all directories to a list to watch
 function getDirectories (dir, filelist, exclude) {
-    var files = fs.readdirSync(dir);
-    var i = exclude.indexOf(dir)
-    filelist = filelist || [];
-    if (i == -1) {
-        files.forEach(function (file) {
-        if (fs.statSync(dir + '/' + file).isDirectory()) {
-            filelist = getDirectories(dir + '/' + file, filelist, exclude);
-        }
-        else {
-            if (filelist.indexOf(dir)) {
-                filelist.push(dir);
+    return new Promise(function executePromise (resolve, reject) { 
+        var files = fs.readdirSync(dir);
+        var i = exclude.indexOf(dir)
+        filelist = filelist || [];
+        if (i == -1) {
+            files.forEach(function (file) {
+            if (fs.statSync(dir + '/' + file).isDirectory()) {
+                getDirectories(dir + '/' + file, filelist, exclude)
+                    .then(function () {
+                        resolve(filelist);
+                    });
             }
+            else {
+                if (filelist.indexOf(dir)) {
+                    filelist.push(dir);
+                }
+            }
+        });
         }
+        resolve(filelist);
     });
-    }
-    return filelist;
 };
