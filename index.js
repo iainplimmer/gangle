27efab3651e3b10526739df967f9ad7852206930
iain plimmer <iainplimmer@iains-iMac.local>
2016-12-11 11:13:38 +0000

    watches files

--- index.js
+++ index.js
@@ -1,48 +1,32 @@
-var http = require('http')
-var url = require('url')
 var fs = require('fs')
 var path = require('path')
 var watch = require('node-watch');
 var browserSync = require("browser-sync").create('gangle');
 
 'use strict'
-//var foldersToWatch = [
-//    './src'
-//];             
 
-//var watchList = getDirectories('./');
-var excludedDirectories = ['.git','node_modules'];  //  We need to know which directories to include
+var excludedDirectories = ['.//.git','.//node_modules'];  //  We need to know which directories to include
 
-//  Let's get the directories that we want to watch first
-// getDirectories('./').map(function (e) {
-    
-//     var iii = excludedDirectories.indexOf(e);
-//     console.log(iii)
-
-//     getDirectories('./' + e).map(function (e) {
-//         console.log(e)
-//     })
-// });
-
-var getDirectories = function (dir, filelist, excludedDirectories) {
-    var fs = fs || require('fs')
+//  We can traverse the directory tree and add all directories to a list 
+var getDirectories = function (dir, filelist, exclude) {
     var files = fs.readdirSync(dir);
-
+    var i = exclude.indexOf(dir)
     filelist = filelist || [];
-    
-    files.forEach(function (file) {
+    if (i == -1) {
+        files.forEach(function (file) {
         if (fs.statSync(dir + '/' + file).isDirectory()) {
-            filelist = getDirectories(dir + '/' + file, filelist);
+            filelist = getDirectories(dir + '/' + file, filelist, exclude);
         }
         else {
-            filelist.push(file);
+            filelist.push(dir + '/' + file);
         }
     });
+    }
     return filelist;
 };
 
 var filelist = [];
-var iain = getDirectories('./', filelist);
+var iain = getDirectories('./', filelist, excludedDirectories);
 console.log(iain)
 
 /*
@@ -50,19 +34,6 @@ browserSync.init({
     server: ".",
     port: 9001
 });
-*/
-
-//  Returns the directories in the application
-// function getDirectories(srcpath) {
-//     return fs.readdirSync(srcpath).filter(function(file) {
-//         return fs.statSync(path.join(srcpath, file)).isDirectory();
-//     });
-// }
-
-//////console.log(watchList)
-
-/*
-WatchFolder(foldersToWatch[0]);
 */
 
 //  First thing we are going to do is setup a watcher on a particular folder, this will read the
